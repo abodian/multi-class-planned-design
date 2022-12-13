6732632c049e822696806c1eb4541330abb44f29
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-13 17:51:25 +0000

    final commit for the day

--- lib/diary.rb
+++ lib/diary.rb
@@ -29,22 +29,25 @@ class Diary
   end
 
   def find_phone
-    phone_numbers = []
     contents_array = []
     @my_entries.each do |element| 
       contents_array << element.contents
     end
     contents_string = contents_array.join(" ")
-    contents_string.split(" ").each do |entry|
-      if entry.match(/\d{11}/)
-        phone_numbers << entry
-      end
-    end
+    phone_numbers = contents_string.scan(/\d+.\d+.\d+/)
+    print contents_string
     return phone_numbers
   end
 end
 
 # diary = Diary.new
 # entry_1 = DiaryEntry.new("Monday", "Walked the dog, it was great")
+# entry_2 = DiaryEntry.new("Tuesday", "Walked the cat, did not like it as the cat didn't like the rain")
+# entry_3 = DiaryEntry.new("Wednesday", "I met Adam today and he was a complete legend, his number is 07800123456")
+# entry_4 = DiaryEntry.new("Thursday", "I met Alex today, he isn't as cool as Adam, his number is 07800123456")
 # diary.add(entry_1)
-# print diary.find_phone
+# diary.add(entry_2)
+# diary.add(entry_3)
+# diary.add(entry_4)
+
+# diary.find_phone
--- spec/diary_integration_spec.rb
+++ spec/diary_integration_spec.rb
@@ -39,4 +39,16 @@ RSpec.describe "Diary integration" do
     diary.add(entry_3)
     expect(diary.find_phone).to eq ["07800123456"]
   end
+
+  it "returns multiple phone numbers from within entry" do
+    diary = Diary.new
+    entry_1 = DiaryEntry.new("Monday", "Walked the dog, it was great")
+    entry_2 = DiaryEntry.new("Tuesday", "Walked the cat, did not like it as the cat didn't like the rain")
+    entry_3 = DiaryEntry.new("Wednesday", "I met Adam today and he was a complete legend, his number is 07800123456")
+    entry_4 = DiaryEntry.new("Thursday", "I met Alex today, he isn't as cool as Adam, his number is 07800123321")
+    diary.add(entry_1)
+    diary.add(entry_2)
+    diary.add(entry_3)
+    expect(diary.find_phone).to eq ["07800123456", "07800123321"]
+  end
 end
